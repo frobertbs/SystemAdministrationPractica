78175201d704aca4864b9a2d09bebd44566b253e
frobertbs <bfabianrobert@gmail.com>
2022-11-19 18:04:42 +0100

    Menu creado

---
+++ gestiona-prac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "ASO 22/23 - Assignment 6"
+echo "Student Name : Fabian Robert Banu"
+
+echo -e "\n"
+echo "Practical Assignment Management"
+echo "------------------------------"
+
+echo "Menu"
+case=10
+
+while [ $case != "4" ]; do 
+echo -e "\t 1) Program collection of assignment solution"
+echo -e "\t 2) Pack course assignments"
+echo -e "\t 3) See size and date of a course backup file"
+echo -e "\t 4) End program"
+
+echo -e "\n \t Option:"
+
+read case
+done
