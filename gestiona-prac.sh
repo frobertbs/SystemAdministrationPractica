c453284dd277036972c84031390c8cb72004e707
frobertbs <bfabianrobert@gmail.com>
2022-12-09 17:56:26 +0100

    Añadido en los menús informes y condiciones

--- gestiona-prac.sh
+++ gestiona-prac.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-echo "ASO 22/23 - Assignment 6"
-echo "Student Name : Fabian Robert Banu"
+echo "ASO 22/23 - Práctica 6"
+echo "Fabian Robert Banu"
 
 echo -e "\n"
-echo "Practical Assignment Management"
+echo "Gestión de prácticas"
 echo "------------------------------"
 
-echo "Menu"
-case=10
-while [ $case != "4" ]
+echo "Menú"
+opcion=10
+while [ $opcion != "4" ]
 do 
 	echo -e "\n"
-	echo -e "\t 1) Program collection of assignment solution"
-	echo -e "\t 2) Pack course assignments"
-	echo -e "\t 3) See size and date of a course backup file"
-	echo -e "\t 4) End program"
+	echo -e "\t 1) Programar recogida de prácticas"
+	echo -e "\t 2) Empaquetado de prácticas de una asignatura"
+	echo -e "\t 3) Ver tamaño y fecha del fichero de una asignatura"
+	echo -e "\t 4) Finalizar programa"
 
-	echo -e "\n \t Option:"
+	echo -e "\n \t Opción:"
 	read case
 	case $case in 
 	1)
@@ -24,12 +24,29 @@ do
 		echo "Asignatura cuyas prácticas desea recoger: "
 		read asignatura
 		echo "Ruta con las cuentas de los alumnos: "
-		read rutaCuentasAlumnos
+		read directorio_origen
+		if [ ! -d "$directorio_origen" ]; then
+			echo "El directorio_origen no existe" >> informe-prac.log 2>&1
+			echo "Porfavor introduce un directorio que exista: "
+			read directorio_origen
+		fi
 		echo "Ruta para almacenar prácticas: "
-		read rutaAlmacenamientoPrac
-		echo "Se va a programar la recogida de las prácticas de ASO para mañana a las 8.00. Origen:$rutaCuentasAlumnos. Destino: $rutaAlmacenamientoPrac "
+		read directorio_destino
+		if [ ! -d $directorio_destino ]; then
+			$(mkdir $directorio_destino)
+		fi
+		echo "Se va a programar la recogida de las prácticas de ASO para mañana a las 8.00. Origen:$directorio_origen. Destino: $directorio_destino "
 		echo "Está de acuerdo (s/n)?"
-		read confirmacion
+		read acuerdo
+		if [ $acuerdo -eq "s" ]; then
+			sudo crontab -l > cron_bkp
+			sudo echo "0 8 * * * sudo /home/recoge-prac.sh >/dev/null 2>&1" >> cron_bkp
+			sudo crontab cron_bkp
+			sudo rm cron_bkp
+			./recoge-prac.sh $directorio_origen $directorio_destino
+		fi
+		
+
 		;;
 	2)
 		echo "Menú 2 - Empaquetar prácticas en la asignatura"
@@ -38,14 +55,21 @@ do
 		echo "Ruta absoluta del directorio de prácticas: "
 		read rutaAbsDirPrac
 		echo "Se van a empaquetar las prácticas de la asignatura $asignaturaEmpaq presentes en el directorio $rutaAbsDirPrac"
+		nombreArchivo=$asignatura-$(date '+%y%m%d')
 		echo "¿Está de acuerdo? (s/n)"
 		read confirmacionEmpaquetado
+		if [ confirmacionEmpaquetado == "s" ]; then
+			tar -cvzf $nombreArchivo.tgz $rutaAbsDirPrac
+		fi
 		;;
 		
 	3)
 		echo "Menú 3 – Obtener tamaño y fecha del fichero"
 		echo "Asignatura sobre la que queremos información: "
 		read infoAsignatura
+		nombreArchivo=$asignatura-$(date '+%y%m%d')
 		echo "El fichero generado es aso-221013.tgz y ocupa <n> bytes"
+		tar "$nombreArchivo.tar.tgz" 
+		# date '+%y%m%d	// muestra 221209
 	esac
 done
